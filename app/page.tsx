0e3b392da737bd0f6fc78722982435d541d2c589
Tilak Rathoure <tilakrathoure@gmail.com>
2025-07-08 22:56:58 +0530

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Button >Nice</Button>
+      <Input type='text' className='' placeholder='hellog'/></div>
   )
 }
 
