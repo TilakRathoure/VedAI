5a7a248886229e1e9537f2060312dbc38f1769b9
Tilak Rathoure <tilakrathoure@gmail.com>
2025-07-09 22:27:39 +0530

    install packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { ArrowDownCircle, ArrowDownCircleIcon, Loader2, MessageCircle, Send, X } from 'lucide-react'
 import React from 'react'
+import {useChat} from "@ai-sdk/react"
 
 const Home = () => {
   return (
     <div>
-      <Button >Nice</Button>
-      <Input type='text' className='' placeholder='hellog'/></div>
+
+      <X/>
+      <MessageCircle/>
+      <Send/>
+      <Loader2/>
+      <ArrowDownCircleIcon/>
+      
+
+    </div>
   )
 }
 
