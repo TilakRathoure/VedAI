00402450d4c8529f30a84e4de17a3e0a182db269
Tilak Rathoure <tilakrathoure@gmail.com>
2025-07-25 12:14:58 +0530

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Outfit} from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +36,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased ${outfit.className}`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
